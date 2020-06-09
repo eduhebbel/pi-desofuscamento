4dcdccb2970e7be7dc759050726f61a97579b6b4
André H Martello <andre.martello@mercadolivre.com>
2020-06-08 23:28:08 -0300

    Enhance validation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,17 +1,16 @@
 const router = require("express").Router();
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const musers = require('../models/musers');
 
 
 //Inicio do Solicita lista completa de Usuarios
 router.get('/', (req, res) => {
-    
-    musers.findAll().then(musers =>{
-        if(musers.length > 0 ){
+    musers.findAll().then(musers => {
+        if (musers.length > 0) {
             return res.json({
                 data: musers
             });
-        } else{
+        } else {
             res.status(404).json({
                 erros: [{
                     value: '',
@@ -19,120 +18,124 @@ router.get('/', (req, res) => {
                 }]
             });
         }
+    }).catch(erros => {
+        console.log(erros);
+        return res.status(500).json({
+            erros: [{
+                value: '',
+                msg: 'Falha na comunicação com o Banco!'
+            }]
+        });
     });
-
 });//Fim do Solicita lista completa de Usuarios
 
 //Inicio do Solicita usuario por ID
-router.get('/:id', (req, res) => {
-    check('id', '"ID" de ser um numero inteiro!')
-    .trim()
-    .escape()
-    .isInt()
-    .toInt()
-    .notEmpty()
+router.get('/:id', [
+    check('id', '"id de ser um numero inteiro e correspondente à usuário existente!')
+        .trim()
+        .escape()
+        .isInt()
+        .toInt()
+        .notEmpty()
+], (req, res) => {
 
     const erros = validationResult(req);
-    
-    if(erros.isEmpty()){
+
+    if (erros.isEmpty()) {
         musers.findByPk(req.params.id).then(musers => {
-            if(musers){
+            if (musers) {
                 return res.json({
-                data: [musers]
-                    });
-            } else{
+                    data: [musers]
+                });
+            } else {
                 return res.status(404).json({
                     erros: [{
                         value: '',
-                        msg: 'Usuario não encontrado!'
+                        msg: 'Usuario não encontrado com esse id!'
                     }]
                 })
             }
-            }).catch(erros => {
-                console.log(erros);
-                return res.status(500).json({
-                    erros: [{
-                        value: '',
-                        msg: 'Falha na comunicação com o Banco!'
-                    }]
-                });
+        }).catch(erros => {
+            console.log(erros);
+            return res.status(500).json({
+                erros: [{
+                    value: '',
+                    msg: 'Falha na comunicação com o Banco!'
+                }]
             });
-    }else{
+        });
+    } else {
         return res.status(422).json(erros);
-    }//fim di IF-ELSE
+    }
 
 });//Fim do Solicita usuario por ID
 
 //Inicio do Cadastra novo usuario
 router.post('/', [
-    check('nome' , '"Nome" é um campo obrigatorio.',
-    'senha' , '"Senha" é um campo obrigatorio.',
-    'email', '"E-mail" é  um campo obrigatorio')
-    .trim()
-    .escape()
-    .notEmpty(),
-    check(
-    'senha' , '"Senha" é um campo obrigatorio.')
-    .trim()
-    .escape()
-    .notEmpty(),
-    check('email', '"E-mail" é  um campo obrigatorio')
-    .trim()
-    .escape()
-    .notEmpty()
-    ], (req, res) => {
+    check('nome', 'nome é um campo obrigatorio.')
+        .trim()
+        .escape()
+        .notEmpty(),
+    check('senha', 'senha é um campo obrigatorio.')
+        .trim()
+        .escape()
+        .notEmpty(),
+    check('email', 'email, com valor válido, é um campo obrigatorio.')
+        .trim()
+        .escape()
+        .notEmpty()
+        .isEmail()
+], (req, res) => {
     const erros = validationResult(req);
-    
-    if(erros.isEmpty()){
+
+    if (erros.isEmpty()) {
         musers.create({
             nome: req.body.nome,
             senha: req.body.senha,
             email: req.body.email
         }).then(musers => {
             return res.json({
-              data: [musers]
-                });
-            }).catch(erros => {
-                console.log(erros);
-                return res.status(500).json({
-                    erros: [{
-                        value: '',
-                        msg: 'Flha na comunicação com o Banco!'
-                    }]
-                });
+                data: [musers]
             });
-    }else{
+        }).catch(erros => {
+            console.log(erros);
+            return res.status(500).json({
+                erros: [{
+                    value: '',
+                    msg: 'Falha na comunicação com o Banco!',
+                }]
+            });
+        });
+    } else {
         return res.status(422).json(erros);
-    }//fim di IF-ELSE
+    }
 });//Fim do Cadastra novo usuario
 
 //Inicio do Altera um usuario
-router.put('/:id',[
-    check('id', '"ID" de ser um numero inteiro!')
-    .trim()
-    .escape()
-    .isInt()
-    .toInt()
-    .notEmpty(),
-    check('nome' , '"Nome" é um campo obrigatorio.',
-    'senha' , '"Senha" é um campo obrigatorio.',
-    'email', '"E-mail" é  um campo obrigatorio')
-    .trim()
-    .escape()
-    .notEmpty(),
-    check(
-    'senha' , '"Senha" é um campo obrigatorio.')
-    .trim()
-    .escape()
-    .notEmpty(),
-    check('email', '"E-mail" é  um campo obrigatorio')
-    .trim()
-    .escape()
-    .notEmpty()
-] ,(req, res) => {
+router.put('/:id', [
+    check('id', '"id de ser um numero inteiro e correspondente à usuário existente!')
+        .trim()
+        .escape()
+        .isInt()
+        .toInt()
+        .notEmpty(),
+    check('nome', 'nome é um campo obrigatorio.')
+        .trim()
+        .escape()
+        .notEmpty(),
+    check('senha', 'senha é um campo obrigatorio.')
+        .trim()
+        .escape()
+        .notEmpty(),
+    check('email', 'email, com valor válido, é um campo obrigatorio.')
+        .trim()
+        .escape()
+        .notEmpty()
+        .isEmail()
+], (req, res) => {
     const erros = validationResult(req);
-    
-    if(erros.isEmpty()){
+
+    if (erros.isEmpty()) {
         musers.update({
             nome: req.body.nome,
             senha: req.body.senha,
@@ -142,23 +145,23 @@ router.put('/:id',[
                 id: req.params.id
             }
         }).then(rows => {
-            if(rows[0] === 1){
+            if (rows[0] === 1) {
                 return res.json({
                     data: [{
-                      rows: rows[0],
-                      nome: req.body.nome,
-                      senha: req.body.senha,
-                      email: req.body.email  
+                        rows: rows[0],
+                        nome: req.body.nome,
+                        senha: req.body.senha,
+                        email: req.body.email
                     }]
                 })
-            } else if (rows[0] === 0){
+            } else if (rows[0] === 0) {
                 return res.status(404).json({
                     erros: [{
                         value: '',
                         msg: 'Usuario não encontrado!'
                     }]
                 });
-            } else{
+            } else {
                 console.log(erros);
                 return res.status(500).json({
                     erros: [{
@@ -167,52 +170,52 @@ router.put('/:id',[
                     }]
                 });
             }
-            }).catch(erros => {
-                console.log(erros);
-                return res.status(500).json({
-                    erros: [{
-                        value: '',
-                        msg: 'Falha na comunicação com o Banco!'
-                    }]
-                });
+        }).catch(erros => {
+            console.log(erros);
+            return res.status(500).json({
+                erros: [{
+                    value: '',
+                    msg: 'Falha na comunicação com o Banco!'
+                }]
             });
-    }else{
+        });
+    } else {
         return res.status(422).json(erros);
-    }//fim di IF-ELSE
+    }
 });//Fim do Altera um usuario
 
 //Inicio do Deleta um Usuario
-router.delete('/:id',[
-    check('id', '"ID" de ser um numero inteiro!')
-    .trim()
-    .escape()
-    .isInt()
-    .toInt()
-    .notEmpty()
+router.delete('/:id', [
+    check('id', '"id de ser um numero inteiro e correspondente à usuário existente!')
+        .trim()
+        .escape()
+        .isInt()
+        .toInt()
+        .notEmpty()
 ], (req, res) => {
     const erros = validationResult(req);
-    
-    if(erros.isEmpty()){
+
+    if (erros.isEmpty()) {
         musers.destroy({
             where: {
                 id: req.params.id
             }
         }).then(rows => {
-            if(rows === 1){
+            if (rows === 1) {
                 return res.json({
                     data: [{
-                      rows: rows
-                      
+                        rows: rows
+
                     }]
                 })
-            } else if (rows === 0){
+            } else if (rows === 0) {
                 return res.status(404).json({
                     erros: [{
                         value: '',
                         msg: 'Usuario não encontrado!'
                     }]
                 });
-            } else{
+            } else {
                 console.log(erros);
                 return res.status(500).json({
                     erros: [{
@@ -221,16 +224,16 @@ router.delete('/:id',[
                     }]
                 });
             }
-            }).catch(erros => {
-                console.log(erros);
-                return res.status(500).json({
-                    erros: [{
-                        value: '',
-                        msg: 'Falha na comunicação com o Banco!'
-                    }]
-                });
+        }).catch(erros => {
+            console.log(erros);
+            return res.status(500).json({
+                erros: [{
+                    value: '',
+                    msg: 'Falha na comunicação com o Banco!'
+                }]
             });
-    }else{
+        });
+    } else {
         return res.status(422).json(erros);
     }//fim di IF-ELSE
 });//Fim do Deleta um usuario
