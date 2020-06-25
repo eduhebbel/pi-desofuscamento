154151d13beca89d3da234ab8e0080dc133897b5
André H Martello <andrehagemann@hotmail.com>
2020-06-25 18:40:51 -0300

    endpoint que desofusca uma string, não arquivo

--- backend/routes/FileUpload.js
+++ backend/routes/FileUpload.js
@@ -1,20 +1,18 @@
 const router = require("express").Router();
-const { check } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
-const multer =  require("multer");
+const multer = require("multer");
 
 const decodeAll = require('../desofuscamento/index');
 const fs = require('fs');
 const readline = require('readline');
-const { once }  = require("events");
-// console.log(once);
-// console.log(typeof(once));
+const { once } = require("events");
 
 
-const upload = multer ({
+const upload = multer({
     dest: process.env.UPLOAD_DIR,
     fileFilter: (req, file, cb) => {
-        if(file.mimetype != 'text/plain' && !file.mimetype.includes('log')){
+        if (file.mimetype != 'text/plain' && !file.mimetype.includes('log')) {
             return cb(new Error('Formato invalido.'));
         }
         cb(null, true);
@@ -24,32 +22,44 @@ const upload = multer ({
 
 router.post('/file', (req, res) => {
     upload(req, res, async (err) => {
-        if(err) {
+        if (err) {
             console.log(err);
-            res.status(442).send({ erro: 'Formato invalido'});
+            res.status(442).send({ erro: 'Formato invalido' });
         } else {
             let file = req.file;
-
             const path = await processFile(file);
-
-            if(path){
+            if (path) {
                 res.download(path, file.originalname);
             } else {
                 console.log('erro desofuscando arquivo');
-                res.status(500).send({erro: 'erro desofuscando arquivo'});
+                res.status(500).send({ erro: 'erro desofuscando arquivo' });
             }
-            
         }
-       
-
     });
 });
 
 router.post('/text', [
-        check('text', 'uma frase ofuscada é necessária')
+    check('text', 'uma string ofuscada é necessária no campo text')
         .trim()
-],(req, res) => {
-    res.status(501).send({ erro: 'not implemented'});
+        .isString()
+        .notEmpty(),
+], (req, res) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()){
+        res.status(400).json({ errors: validationErrors.errors});
+        return
+    }
+
+    const encodedString = req.body.text;
+    try {
+        const decodedString = decodeAll(encodedString);
+        res.status(200).send({ decodedString: decodedString });
+    } catch {
+        res.status(500).send({ 
+            error: 'error decoding string',
+            encodedString: encodedString
+        });
+    }
 });
 
 async function processFile(file) {
@@ -68,15 +78,15 @@ async function processFile(file) {
     });
 
     readInterface.on('line', (line) => {
-            writeStream.write(`${decodeAll(line)}\n`);
+        writeStream.write(`${decodeAll(line)}\n`);
     });
 
     readInterface.on('close', () => {
         writeStream.end();
     });
-    
+
     await once(writeStream, 'finish');
-    
+
     return outpath;
 }
 
